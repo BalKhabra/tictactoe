858ee6da652db167adf8a83006f6dd929ed0790b
Baljit Khabra <baljitkhabra99@gmail.com>
2022-10-30 17:57:49 -0700

    completed

---
+++ game/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Board from './Board';
+import Square from './Square';
+
+
+function App() {
+  return (
+    <>
+    <Square />
+    <Board />
+    </>
+  );
+}
+
+export default App;
